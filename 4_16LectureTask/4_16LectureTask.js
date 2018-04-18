08697116f2ece4bd3f3bad18026e4815c9928bc7
Ray Lee <Lee31978@gmail.com>
2018-04-18 18:54:17 -0400

    small mod to 4_16LectureTask.js

--- 4_16LectureTask/4_16LectureTask.js
+++ 4_16LectureTask/4_16LectureTask.js
@@ -20,10 +20,9 @@ function orderSupplies(item, callback) {
 	
 	function receivedItem(item) {
 		if (item.product == "Horsehair brush"){
-            setTimeout( console.log(`Received ${item.product}, time to ${item.directions() }`), 3000  );
-            //this def isn't working, but I'm not clear why "call back argument must be a function"
+            setTimeout( function() {console.log(`Received ${item.product}, time to ${item.directions() }`)}, 3000  );
+            //wrapped the console log in an anonymous function and passed into setTimeout()
 		}else{
-            // console.log (item.product);
 			console.log(`Received ${item.product}, time to ${item.directions()}`);
 		}
 	}
@@ -31,10 +30,7 @@ function orderSupplies(item, callback) {
 	
 	orderSupplies('paint', receivedItem);
 	orderSupplies('brush', receivedItem);
-    
-    //setTimeout(orderSupplies('brush', receivedItem), 3000);  
-    //this throws a typeError stating that "callback" argument must be a function. 
-    //Seems that received item becomes out of scope if I use the above method.
+
 	
 	
 	
