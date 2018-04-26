2e429fb97e4115b2d169401e6210bc46730298a1
Ray Lee <Lee31978@gmail.com>
2018-04-26 00:31:40 -0400

    some small edits to the Landing Page app.js

--- NODE/Landing Page/App.js
+++ NODE/Landing Page/App.js
@@ -19,13 +19,13 @@ var server = http.createServer(function (request, response){
         response.write(contents); // send response body
         response.end(); //finished
     });
-    // // request didn't match anything:
-    }else if(request.url === '/dojos/new'){
-        fs.readFile('dojos.html', 'utf8', function(errors, contents){
-            response.writeHead(200, {'Content-Type': 'text/html'}); //send data about response
-            response.write(contents); //send response body
-            response.end(); //finished
-        });
+}else if(request.url === '/dojos/new'){
+    fs.readFile('dojos.html', 'utf8', function(errors, contents){
+        response.writeHead(200, {'Content-Type': 'text/html'}); //send data about response
+        response.write(contents); //send response body
+        response.end(); //finished
+    });
+    // // request didn't match anything
     }else{
         fs.readFile('errors.html', "utf8", function(errors, contents){
         response.writeHead(200, {'Content-Type': "text/html"}); //send data about response
---
+++ NODE/readme.txt
@@ -0,0 +1,1 @@
+This is a simple container folder for Node Projects
