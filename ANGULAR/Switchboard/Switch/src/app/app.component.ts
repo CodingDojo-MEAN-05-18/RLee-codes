e555e7beb86873be3fd792e314ac5c64b174fb60
Ray Lee <Lee31978@gmail.com>
2018-06-18 04:33:54 -0400

    Switchboard app added

---
+++ ANGULAR/Switchboard/Switch/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Switchboard!';
+
+  length: number;
+  board = {};
+  boardKeys = [];
+  error = '';
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.board = {};
+    this.boardKeys = [];
+    this.error = '';
+    if (this.length <= 0) {
+      this.error = "PLEASE ENTER A VALID VALUE. (number > 0)"
+    } else {
+      for (let x = 1; x <= this.length; x++) {
+        this.board[x] = true;
+        this.boardKeys.push(x);
+      }
+      console.log("Submitting form", this.board);
+    }
+  }
+
+  onSwitchFlip(number) {
+    if (this.board[number] == true) {
+      this.board[number] = false;
+    } else {
+      this.board[number] = true;
+    }
+  }
+
+}
---
+++ ANGULAR/Switchboard/readme.txt
@@ -0,0 +1,8 @@
+Switchboard
+Create a row of switches. Upon clicking any one of the switches, change the switch from 'ON' text
+with a green background, to 'OFF' text and red background. 
+
+When we click each indicidual button, the button should toggle from ON(green) to OFF(red).
+The initial order of on/off position is not relevant.
+Create a row of 10 buttons.
+    **For practice, a form element was added which allows the user to select the number of switches.
