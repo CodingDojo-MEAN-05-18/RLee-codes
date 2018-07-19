b67fd7bfdf5178d7d3c5a9ef25202ac21177d3ed
Ray Lee <Lee31978@gmail.com>
2018-07-19 01:50:14 -0400

    practicing with Express / Angular integrations.

---
+++ Integration/IntegrationPractice/AngularApp/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  {
+    path: '/',
+    pathMatch: 'full',
+    component: AppComponent
+  },
+  {
+    path: '*',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ Integration/IntegrationPractice/server.js
@@ -0,0 +1,37 @@
+// require express
+var express = require('express');
+var app = express();
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+// set the path such that the dist folder is used for static content.
+const path = require('path');
+app.use(express.static(path.join(__dirname, '/Angularpp/dist')));
+
+
+// //mongoose module  //NOT NEEDED YET
+// var mongoose = require('mongoose');
+// //connect mongoose to mongoDB
+// mongoose.connect('mongodb://localhost/DBNAME');
+// //use native promises
+// mongoose.Promise = global.Promise;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+// include the json format for integrations
+app.use(bodyParser.json());
+
+
+// NO TEMPLATING ENGINE NEEDED 
+// // setting up ejs and our views folder
+// app.set('views', path.join(__dirname, './views'));
+// app.set('view engine', 'ejs');
+
+
+module.exports = function (app) {
+    app.all("*", (req, res, next) => {
+        res.sendFile(path.resolve("./AngularApp/dist/index.html"));
+    });
+};
